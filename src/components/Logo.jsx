9bf8a9d8929a907c62622a791652ba97909c5f0e
Billyp137 <contact@parkerexpress.com.au>
2026-04-26 03:10:39 +1000

    Fix: refine PX logo SVG shapes

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,24 +4,14 @@ export default function Logo({ size = 'md', className = '' }) {
   const textSize = size === 'sm' ? 'text-sm' : size === 'lg' ? 'text-xl' : 'text-base'
   return (
     <div className={`flex items-center gap-2.5 ${className}`}>
-      <svg width={px} height={Math.round(px * 0.83)} viewBox="0 0 120 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-        {/* ===== P LETTER ===== */}
-        {/* Gold: left vertical bar of P */}
-        <polygon points="5,20 20,20 20,95 5,95" fill="#F5A623"/>
-        {/* Gold: middle horizontal arm of P (bowl midpoint) */}
-        <polygon points="5,45 55,45 55,60 5,60" fill="#F5A623"/>
-        {/* Black: top horizontal bar of P (overlays gold, full width of P) */}
-        <polygon points="5,5 70,5 70,20 5,20" fill="#111111"/>
-        {/* Black: right side vertical of P bowl */}
-        <polygon points="55,5 70,5 70,60 55,60" fill="#111111"/>
-
-        {/* ===== X LETTER ===== */}
-        {/* Black: main left diagonal of X (bottom-left to top-right, thick) */}
-        <polygon points="25,95 42,95 90,5 73,5" fill="#111111"/>
-        {/* Black: lower-right arm of X */}
-        <polygon points="73,95 90,95 66,58 49,58" fill="#111111"/>
-        {/* Gold: upper-right arm of X */}
-        <polygon points="90,5 107,5 83,38 66,38" fill="#F5A623"/>
+      <svg width={px} height={px} viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect x="4" y="22" width="15" height="68" fill="#F5A623"/>
+        <rect x="4" y="44" width="52" height="14" fill="#F5A623"/>
+        <polygon points="76,4 94,4 70,36 52,36" fill="#F5A623"/>
+        <rect x="4" y="4" width="62" height="18" fill="#111"/>
+        <rect x="51" y="4" width="15" height="54" fill="#111"/>
+        <polygon points="20,96 36,96 86,4 70,4" fill="#111"/>
+        <polygon points="70,96 86,96 62,58 46,58" fill="#111"/>
       </svg>
       <div className={`font-condensed font-bold tracking-wide leading-none ${textSize}`}>
         <span className="text-white">PARKER</span>
